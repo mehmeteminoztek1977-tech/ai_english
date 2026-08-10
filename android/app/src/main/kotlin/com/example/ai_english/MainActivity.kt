acef971d471b13e2d79950967aa5c551c54f5513
Server Admin <admin@server.local>
2026-08-10 15:36:48 +0300

    Regenerate modern Android Gradle project structure

--- android/app/src/main/kotlin/com/example/ai_english/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ai_english/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.server.aienglish.ai_english
+package com.example.ai_english
 
 import io.flutter.embedding.android.FlutterActivity
 
--- android/app/src/main/kotlin/com/onofis/ai_english/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.onofis.ai_english
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
